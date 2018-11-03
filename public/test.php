4eac877f160a0f9155a4c4e8f7d2166ccd9f8de2
barakat_abweh <bararkat.abweh.96@gmail.com>
2018-11-03 22:52:21 +0200

    reset password almost ready

---
+++ public/test.php
@@ -0,0 +1,11 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+echo $_SERVER['SERVER_NAME']."<br>".$_SERVER['PHP_SELF']."<br>". $_SERVER['DOCUMENT_ROOT']."<br>".$_SERVER["REMOTE_ADDR"] ;
+
+?>
