7e7780c2c475673aaa5d3c1b16f5aef07af747ab
barakat_abweh <bararkat.abweh.96@gmail.com>
2018-11-24 17:42:22 +0200

    buyer notification ready

--- includes/buyernotifications.php
+++ includes/buyernotifications.php
@@ -19,19 +19,43 @@ if(htmlspecialchars($_SERVER['REQUEST_METHOD'])=="POST"){
                 while($res=$database->fetchArray($Result)){
                $user->setId($res['interested_user_id']);
                $property->setId($res['property_id']);
-                $result.="<tr><td>".$property->getName()."</td><td>".$property->getType()."</td><td><a href='profile.php?user_id='".$res['interested_user_id']."'>".$user->getUname()."</a></td><td>".$res['Date']."</td><td><button id='accept' onclick=\"accept('".$res['interest_id']."');\">Accept!</button></td><td><button id='reject' onclick=\"reject('".$res['interest_id']."');\">Reject!</button></td><td><button id='init' onclick=\"inimessage('".$res['interest_id']."');\">Send Initial Message!</button></td>"."</tr>";
+               $canceled="Not Defined";
+               $accorrej="Not Defined";
+               if($res['canceled']=="1"){
+                   $canceled="canceled";
+               }else{
+                   if($res['accepted']=="1"){
+                       $accorrej="Accepted";
+                   }
+                   else if($res['accepted']=="2"){
+                       $accorrej="Rejected";
+                   }
+               }
+                $result.="<tr><td>".$property->getName()."</td><td>".$property->getType()."</td><td><a href='profile.php?user_id='".$res['interested_user_id']."'>".$user->getUname()."</a></td><td>".$res['Date']."</td><td>$accorrej</td><td>$canceled</td>"."</tr>";
                 $query="UPDATE `interested` SET `seen1`= 1 WHERE `interest_id`=".$res['interest_id'];       
                 $database->query($query);
                 }
         }
     else if($id=="1"){
-        $query="SELECT `interest_id`, `interested_user_id`,`Date`, `property_id` FROM `interested` WHERE `property_owner_id`='$userid' AND `accepted` = 0 AND `canceled` = 0 AND interest_id >= $id AND `seen1` = 0 ORDER BY `Date` DESC";
-                $Result=$database->query($query);
-                while($res=$database->fetchArray($Result)){
+                 $query="SELECT `property_owner_id`,`Date`, `property_id`, `accepted`, `canceled` FROM `interested` WHERE `interested_user_id`='$userid' AND interest_id >= $id AND `seen1` = 0 ORDER BY `Date` DESC";
+              $Result=$database->query($query);
+while($res=$database->fetchArray($Result)){
                $user->setId($res['interested_user_id']);
                $property->setId($res['property_id']);
-                $result.="<tr><td>".$property->getName()."</td><td>".$property->getType()."</td><td><a href='profile.php?user_id='".$res['interested_user_id']."'>".$user->getUname()."</a></td><td>".$res['Date']."</td><td><button id='accept' onclick=\"accept('".$res['interest_id']."');\">Accept!</button></td><td><button id='reject' onclick=\"reject('".$res['interest_id']."');\">Reject!</button></td><td><button id='init' onclick=\"inimessage('".$res['interest_id']."');\">Send Initial Message!</button></td>"."</tr>";
-                $query="UPDATE `interested` SET `seen`= 1 WHERE `interest_id`=".$res['interest_id'];       
+               $canceled="Not Defined";
+               $accorrej="Not Defined";
+               if($res['canceled']=="1"){
+                   $canceled="canceled";
+               }else{
+                   if($res['accepted']=="1"){
+                       $accorrej="Accepted";
+                   }
+                   else if($res['accepted']=="2"){
+                       $accorrej="Rejected";
+                   }
+               }
+                $result.="<tr><td>".$property->getName()."</td><td>".$property->getType()."</td><td><a href='profile.php?user_id='".$res['interested_user_id']."'>".$user->getUname()."</a></td><td>".$res['Date']."</td><td>$accorrej</td><td>$canceled</td>"."</tr>";
+                $query="UPDATE `interested` SET `seen1`= 1 WHERE `interest_id`=".$res['interest_id'];       
                 $database->query($query);
                 }
     }else{
