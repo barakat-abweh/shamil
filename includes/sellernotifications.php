990125ddbd4165858024eec089b5206ed1974c35
barakat_abweh <bararkat.abweh.96@gmail.com>
2018-11-24 17:56:00 +0200

    fixed issues in seller notification

--- includes/sellernotifications.php
+++ includes/sellernotifications.php
@@ -14,7 +14,7 @@ if(htmlspecialchars($_SERVER['REQUEST_METHOD'])=="POST"){
         $user->setId($userid);
         $id=$database->escape(trim(htmlspecialchars($_POST['lastinterestid'])));
         if($id=="0"){
-         $query="SELECT `interest_id`, `interested_user_id`,`Date`, `property_id` FROM `interested` WHERE `interested_user_id`='$userid' AND `accepted` = 0 AND `canceled` = 0 AND interest_id >= $id ORDER BY `Date` DESC";     
+         $query="SELECT `interest_id`, `interested_user_id`,`Date`, `property_id` FROM `interested` WHERE `property_owner_id`='$userid' AND interest_id >= $id ORDER BY `Date` DESC";     
          $Result=$database->query($query);
                 while($res=$database->fetchArray($Result)){
                $user->setId($res['interested_user_id']);
@@ -24,9 +24,9 @@ if(htmlspecialchars($_SERVER['REQUEST_METHOD'])=="POST"){
                 $database->query($query);
                 }
         }
-   /* else if($id=="1"){
-        $query="SELECT `interest_id`, `interested_user_id`,`Date`, `property_id` FROM `interested` WHERE `property_owner_id`='$userid' AND `accepted` = 0 AND `canceled` = 0 AND interest_id >= $id AND `seen` = 0 ORDER BY `Date` DESC";
-                $Result=$database->query($query);
+    else if($id=="1"){
+         $query="SELECT `interest_id`, `interested_user_id`,`Date`, `property_id` FROM `interested` WHERE `property_owner_id`='$userid' AND interest_id >= $id AND `seen` = 0 ORDER BY `Date` DESC";     
+        $Result=$database->query($query);
                 while($res=$database->fetchArray($Result)){
                $user->setId($res['interested_user_id']);
                $property->setId($res['property_id']);
@@ -37,7 +37,7 @@ if(htmlspecialchars($_SERVER['REQUEST_METHOD'])=="POST"){
     }
     else{
         redirect();
-    }*/
+    }
         echo $result;
     }
 }
