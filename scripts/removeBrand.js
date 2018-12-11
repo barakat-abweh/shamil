59c53cc8d1206da1c68840e5cfeda572cf3b1440
barakat_abweh <bararkat.abweh.96@gmail.com>
2018-12-11 18:47:54 +0200

    working on admin panel

--- scripts/removeBrand.js
+++
@@ -1,8 +0,0 @@
-setTimeout(function(){ remove(); }, 800);
-function remove(){
-var elements = document.getElementsByTagName('div');
-var length = document.getElementsByTagName('div').length;
-var target = elements[length-1];
-target.remove();
-
-}
