03e45c2582b11e7c51024eb1f633dd18e449eeaf
unknown <F.qzieh@gmail.com>
2018-12-12 12:20:35 -0800

    admin

--- public/DODA.php
+++ public/DODA.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
-    <head></head>
+    <head>
+            <link rel="stylesheet" type="text/css" href="../styles/bootstrap.min.css"/>
+            <link rel="stylesheet" type="text/css" href="../styles/DODA.css"/>
+    
+    </head>
     <body>
-        <p>Hi, Your account has been either deactivated or deleted.
+        <div class="p-3 mb-2 bg-warning text-dark notes">
+            Hi, Your account has been either deactivated or deleted.
         for more information please contact our admin at info@shamil.ps .
-        </p>
+        </div>
+
+       
     </body>
 </html>
